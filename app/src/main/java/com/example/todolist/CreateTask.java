1559106f6bd49d8bcf3efaf7221b3cf1eca975d2
Carlos Del Carpio <cdelcarp@uncc.edu>
2021-02-11 22:17:54 -0500

    create task layout complete

---
+++ app/src/main/java/com/example/todolist/CreateTask.java
@@ -0,0 +1,15 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+        setTitle(getString(R.string.create_task));
+    }
+}
