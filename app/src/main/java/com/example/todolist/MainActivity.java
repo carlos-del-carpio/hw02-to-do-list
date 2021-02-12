31c3887e0d60e2c30fa92ec6accff7b8bc1c9e41
Carlos Del Carpio <cdelcarp@uncc.edu>
2021-02-11 21:24:34 -0500

    added updateTaskCount method

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Task> Tasks;
+    TextView numberOfUpcomingTasksLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Tasks = new ArrayList<>();
+        updateTaskCount(Tasks.size());
+    }
+
+    public void updateTaskCount (int count) {
+        String numberOfTasks;
+        numberOfUpcomingTasksLabel = findViewById(R.id.numberOfUpcomingTasksLabel);
+        numberOfTasks = getString(R.string.you_have) + " " + String.valueOf(count) + " " + getString(R.string.tasks);
+        numberOfUpcomingTasksLabel.setText(numberOfTasks);
     }
 }
